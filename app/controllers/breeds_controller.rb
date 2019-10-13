632638d677e4e72a972019f6e6dc6974e2225a99
Hammy Wallace <Hammy.h.wallace@gmail.com>
2019-10-13 20:50:02 +0000

    Final

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -2,4 +2,19 @@ class BreedsController < ApplicationController
   def index
     @breed = Breed.order("RANDOM()").first
   end
+
+  def new
+    @breed = Breed.new
+  end
+
+  def update
+    Breed.create(breed_params)
+    redirect_to root_path
+  end
+
+private
+
+  def breed_params
+    params.require(:breed).permit(:favorite, :author)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'breeds#index'
+  resources :breeds
 end
